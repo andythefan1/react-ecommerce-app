129aa6ad1f75636efb0b598f4a8de23fd6919958
Andy Fan <andyfan@Andys-MacBook-Pro.local>
2022-04-29 16:03:09 -0500

    Initial commit for crown-clothing ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+const App = () => {
+	const categories = [
+		{ id: 1, title: 'Hats' },
+		{ id: 2, title: 'Jackets' },
+		{ id: 3, title: 'Sneakers' },
+		{ id: 4, title: "Women's" },
+		{ id: 5, title: "Men's" },
+	];
+
+	return (
+		<div className='categories-container'>
+			{categories.map((category) => (
+				<div className='category-container'>
+					{/* <img src="" alt="" /> */}
+					<div className='category-body-container'>
+						<h2>{category.title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
