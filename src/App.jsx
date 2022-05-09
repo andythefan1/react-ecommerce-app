7b217f3669d64fc65b40a72c23c70d7b8b960def
Andy Fan <andyfan@Andys-MacBook-Pro.local>
2022-05-09 16:15:51 -0500

    add firebase package; add sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Dashboard from './routes/Dashboard';
 import Navigation from './routes/Navigation';
+import SignIn from './routes/Sign-in';
 
 const Shop = () => {
 	return <h1>I'm the placeholder for the shop page!</h1>;
@@ -14,6 +15,7 @@ const App = () => {
 				{/* index: a child route with no path that renders parent's outlet */}
 				<Route index element={<Dashboard />} />
 				<Route path='shop' element={<Shop />} />
+				<Route path='sign-in' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
