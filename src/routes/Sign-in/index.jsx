ccc5eb929e4cb4a6b9a4c11ea72b2834fcc5ada6
Andy Fan <andyfan@Andys-MacBook-Pro.local>
2022-05-12 16:32:57 -0500

    implementing redirecting after sign in

--- src/routes/Sign-in/index.jsx
+++ src/routes/Sign-in/index.jsx
@@ -1,18 +1,36 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+	auth,
 	createUserDocumentFromAuth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 } from '../../utils/firebase/utils';
 
 const SignIn = () => {
+	useEffect(() => {
+		return async () => {
+			const response = await getRedirectResult(auth);
+			if (response) {
+				const userDocRef = await createUserDocumentFromAuth(response.user);
+			}
+		};
+	}, []);
+
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(user);
+		console.log(userDocRef);
 	};
 
 	return (
 		<div>
 			<h1>Sign-in Page</h1>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>
+				Sign in with Google Redirect
+			</button>
 		</div>
 	);
 };
