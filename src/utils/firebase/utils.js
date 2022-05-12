2c0efb6f068cd4efa4e304d01056f7e5764d5ca7
Andy Fan <andyfan@Andys-MacBook-Pro.local>
2022-05-12 15:54:35 -0500

    implement creating new users

--- src/utils/firebase/utils.js
+++ src/utils/firebase/utils.js
@@ -6,6 +6,8 @@ import {
 	GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: 'AIzaSyCz-19WWRvYh7IXlgpSgKKV-ywGNHK9sBc',
@@ -19,9 +21,35 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
+
 provider.setCustomParameters({
 	prompt: 'select_account',
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+};
