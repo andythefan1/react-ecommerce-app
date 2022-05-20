20197a5aa47c3740f374ec7255e6cb631e97578c
Andy <andythefan@gmail.com>
2022-05-20 16:21:59 -0500

    implement sign in with email and password via firebase

---
+++ src/components/SignInForm/index.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+
+import {
+	createUserDocumentFromAuth,
+	signInWithGooglePopup,
+	signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/utils';
+
+import './styles.scss';
+import FormInput from '../FormInput';
+import Button from '../Button';
+
+const defaultFormFields = {
+	email: '',
+	password: '',
+};
+
+const SignInForm = () => {
+	const [formFields, setFormFields] = useState(defaultFormFields);
+	const { email, password } = formFields;
+
+	const resetFormFields = () => {
+		setFormFields(defaultFormFields);
+	};
+
+	const signInWithGoogle = async () => {
+		const { user } = await signInWithGooglePopup();
+		await createUserDocumentFromAuth(user);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const { email, password } = formFields;
+
+		// create the user using firebase authentication
+		try {
+			const response = await signInAuthUserWithEmailAndPassword(
+				email,
+				password
+			);
+			console.log(response);
+
+			// clear out the form after success
+			resetFormFields();
+		} catch (error) {
+			let message = '';
+			switch (error.code) {
+				case 'auth/user-not-found':
+					message =
+						'Provided email does not have an account. Please try again.';
+					break;
+				case 'auth/wrong-password':
+					message = 'Provided password is incorrect. Please try again.';
+					break;
+				default:
+					message = 'User sign in encountered an error';
+			}
+
+			alert(message);
+			console.error('User sign in encountered an error', error);
+		}
+	};
+
+	// we want this handler to be generic to handle any input
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		// only want to change the field that was updated
+		setFormFields({ ...formFields, [name]: value });
+	};
+
+	return (
+		<div className='sign-up-container'>
+			<h2>I already have an account</h2>
+			<span>Sign in with your existing email and password</span>
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					label='Email'
+					name='email'
+					onChange={handleChange}
+					required
+					type='text'
+					value={email}
+				/>
+				<FormInput
+					label='Password'
+					name='password'
+					onChange={handleChange}
+					type='password'
+					value={password}
+				/>
+			</form>
+			<div className='buttons-container'>
+				<Button children='Sign in' onClick={handleSubmit} type='submit' />
+				<Button
+					buttonType='google'
+					children='Google Sign in'
+					onClick={signInWithGoogle}
+					type='button'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default SignInForm;
--- src/utils/firebase/utils.js
+++ src/utils/firebase/utils.js
@@ -3,6 +3,7 @@ import {
 	createUserWithEmailAndPassword,
 	getAuth,
 	GoogleAuthProvider,
+	signInWithEmailAndPassword,
 	signInWithRedirect,
 	signInWithPopup,
 } from 'firebase/auth';
@@ -66,4 +67,9 @@ export const createUserDocumentFromAuth = async (
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
 	if (!email || !password) return;
 	return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+	return await signInWithEmailAndPassword(auth, email, password);
 };
