d3ed63e9499cbaa51e1e2392f91a25241a931304
Andy <andythefan@gmail.com>
2022-05-20 16:21:21 -0500

    refactored sign-in page to authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Dashboard from './routes/Dashboard';
 import Navigation from './routes/Navigation';
-import SignIn from './routes/Sign-in';
+import Authentication from './routes/Authentication';
 
 const Shop = () => {
 	return <h1>I'm the placeholder for the shop page!</h1>;
@@ -15,7 +15,7 @@ const App = () => {
 				{/* index: a child route with no path that renders parent's outlet */}
 				<Route index element={<Dashboard />} />
 				<Route path='shop' element={<Shop />} />
-				<Route path='sign-in' element={<SignIn />} />
+				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
 	);
