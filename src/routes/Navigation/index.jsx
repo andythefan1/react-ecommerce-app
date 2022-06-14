347152af3a059aaed1b093e6ef18d55dc98766dc
Andy <andythefan@gmail.com>
2022-06-14 16:17:18 -0500

    adding Styled Components; refactoring Navigation to use styled components

--- src/routes/Navigation/index.jsx
+++ src/routes/Navigation/index.jsx
@@ -7,34 +7,36 @@ import { CartContext } from '../../contexts/CartContext';
 import { UserContext } from '../../contexts/UserContext';
 import { signOutUser } from '../../utils/firebase/utils';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import './styles.scss';
+
+import {
+	LogoContainer,
+	NavigationContainer,
+	NavLinks,
+	NavLink,
+} from './styles';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
 	const { isCartOpen } = useContext(CartContext);
 	return (
 		<Fragment>
-			<div className='navigation'>
-				<Link className='logo-container' to='/'>
+			<NavigationContainer>
+				<LogoContainer to='/'>
 					<CrwnLogo className='logo' />
-				</Link>
-				<div className='nav-links-container'>
-					<Link className='nav-link' to='/shop'>
-						SHOP
-					</Link>
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to='/shop'>SHOP</NavLink>
 					{currentUser ? (
-						<span className='nav-link' onClick={signOutUser}>
+						<NavLink as='span' onClick={signOutUser}>
 							SIGN OUT
-						</span>
+						</NavLink>
 					) : (
-						<Link className='nav-link' to='/auth'>
-							SIGN IN
-						</Link>
+						<NavLink to='/auth'>SIGN IN</NavLink>
 					)}
 					<CartIcon />
-				</div>
+				</NavLinks>
 				{isCartOpen && <CartDropdown />}
-			</div>
+			</NavigationContainer>
 			{/* Renders sub-route component */}
 			<Outlet />
 		</Fragment>
